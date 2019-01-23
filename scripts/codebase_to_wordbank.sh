cbe444d4ecadcb0397de4a719730ea375eb93e8d
lesnyrumcajs <lesny.rumcajs@gmail.com>
2019-01-23 23:37:36 +0100

    ignore blobs for wordbank

--- scripts/codebase_to_wordbank.sh
+++ scripts/codebase_to_wordbank.sh
@@ -1,1 +1,1 @@
-grep -rho -E '[a-zA-Z]{2,15}' . | LC_ALL=C grep -v -P [$'\x80'-$'\xFF'] | sort | uniq -c | sort -nr | head -200 | awk '{print $2}'
+grep -rwho -E '[a-zA-Z]{2,15}' . | LC_ALL=C grep -v -P [$'\x80'-$'\xFF'] | sort | uniq -c | sort -nr | head -200 | awk '{print $2}'
